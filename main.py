c7e39598e82da97e458bff4b3a60750dbae95737
pavan kalyan <srao50872@gmail.com>
2026-03-02 13:02:53 +0530

    update the cors

--- main.py
+++ main.py
@@ -287,7 +287,7 @@ ALLOWED_ORIGINS = [
 ]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://hr-module-pavan-branch.netlify.app","http://localhost:5173","https://recruit.pulsepharma.net"],
+    allow_origins=["https://hr-module-feature-branch.netlify.app","http://localhost:5173","https://recruit.pulsepharma.net"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
